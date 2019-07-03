7eb2ade4c989a99c21e71a7a0cdcb86c47b0825b
Abdellah Ramadan, Josh Mabry and Sam Cook <abdellah@>
2019-07-03 10:36:15 -0700

    add basic file structure and api call

---
+++ src/weather-service.js
@@ -0,0 +1,17 @@
+export class WeatherService {
+  getWeatherByCity(city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=688ccf81582f9e912922ff138e75a0d3`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
